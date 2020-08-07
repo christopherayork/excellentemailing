a46771f575c350a77d0b2667f45dd8d12fd76683
Timothy McGowen <mcgowen17@yahoo.com>
2020-08-07 12:03:05 -0700

    Added the correct regex for the username validation

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,24 +6,28 @@ const secret = require('../config/secret');
 
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S@\S\.\S/, 'is invalid'], index: true },
+    username: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true },
     email: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S@\S\.\S/, 'is invalid'], index: true },
     hash: String,
     salt: String
 }, {timestamps: true})
 
+// Plugin that will check to see if a email/username is already being used.
 UserSchema.plugin(uniqueValidator, { message: 'is already taken.' });
 
+// Function that incrypts the password
 UserSchema.methods.setPassword = function(password) {
     this.salt = crypto.randomBytes(16).toString('hex');
     this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
 };
 
+// Function to validate Password
 UserSchema.methods.validPassword = function(password) {
     var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
     return this.hash === hash;
 };
 
+// Generates a JWT token to send to the front end
 UserSchema.methods.generateJWT = function() {
     var today = new Date();
     var exp = new Date(today);
@@ -36,6 +40,7 @@ UserSchema.methods.generateJWT = function() {
     }, secret);
 };
 
+// 
 UserSchema.methods.toAuthJSON = function() {
     return {
         username: this.username,
