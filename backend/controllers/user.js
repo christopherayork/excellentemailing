51bd78bc7a2fce1f5ca3be654aa257f914ec84ff
Timothy McGowen <mcgowen17@yahoo.com>
2020-08-06 20:28:19 -0700

    Added a basic user and an email list

---
+++ backend/controllers/user.js
@@ -0,0 +1,21 @@
+const UserSchema = require('../models/user');
+
+const createUser = (req, res) => {
+
+
+    res.status(200).json({message: 'User Created'})
+};
+
+const getUser = (req, res) => {
+    res.status(200).json({UserId: req.params.id})
+};
+
+const updateUser = (req, res) => {
+    res.status(200).json({ UserId: req.params.id })
+};
+
+const deleteUser = (req, res) => {
+    res.status(200).json({ UserId: req.params.id })
+};
+
+module.exports = { createUser, getUser, updateUser, deleteUser };
