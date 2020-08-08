3579ec29053aecabc2c78c812fab80e72a9e735e
Timothy McGowen <mcgowen17@yahoo.com>
2020-08-07 19:35:44 -0700

    Changes

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,12 +1,14 @@
 const UserSchema = require('../models/user');
 
 const createUser = (req, res) => {
-    let userData = req.body;
-    let newUser = new UserSchema(userData);
-    newUser.save(function(err) {
-       if(err) res.status(400).json({ message: 'Unable to save post', error: true });
-       else res.status(200).json({ message: 'User created' });
-    });
+    // let userData = req.body;
+    // let newUser = new UserSchema(userData);
+    // newUser.save(function(err) {
+    //    if(err) res.status(400).json({ message: 'Unable to save post', error: true });
+    //    else res.status(200).json({ message: 'User created' });
+    // });
+
+    
 };
 
 const getUser = (req, res) => {
