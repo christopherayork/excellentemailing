4a61137b5eefb471f2f4d5acb7841031189c1c48
christopherayork <christopherayork1992@gmail.com>
2020-08-11 20:01:49 -0400

    Adding endpoints for email CRUD

--- backend/controllers/emails.js
+++ backend/controllers/emails.js
@@ -1,8 +1,12 @@
 const EmailsSchema = require('../models/Emails');
 
 const createEmails = (req, res) => {
-    res.status(200).json({ message: 'Emails Created' });
-
+    if(!req.body || !req.body.hasOwnProperty('email')) res.status(400).json({ message: 'You must supply an email in the format of { email }', error: true });
+    let Email = new EmailsSchema(req.body);
+    Email.save(function(err, obj) {
+        if(err) res.status(400).json({ message: 'The email could not be saved.', error: true });
+        else res.status(200).json({ message: 'The email was added successfully' });
+    });
 };
 
 const getEmails = (req, res) => {
