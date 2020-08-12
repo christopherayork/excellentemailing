302da32a0eab527ac17cad7e1f88961340fd5266
christopherayork <christopherayork1992@gmail.com>
2020-08-11 20:48:59 -0400

    Fixed one warning

--- backend/controllers/emails.js
+++ backend/controllers/emails.js
@@ -3,7 +3,7 @@ const EmailsSchema = require('../models/Emails');
 const createEmails = (req, res) => {
     if(!req.body || !req.body.hasOwnProperty('email')) res.status(400).json({ message: 'You must supply an email in the format of { email }.', error: true });
     let Email = new EmailsSchema(req.body);
-    Email.save(function(err, obj) {
+    Email.save(function(err) {
         if(err) res.status(400).json({ message: 'The email could not be saved.', error: true });
         else res.status(200).json({ message: 'The email was added successfully.' });
     });
