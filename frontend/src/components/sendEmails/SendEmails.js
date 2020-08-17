8001793d198b23c6b09a8247cf874d8434e4108b
Timothy McGowen <mcgowen17@yahoo.com>
2020-08-17 15:14:58 -0700

    added the first part of an email form

--- frontend/src/components/sendEmails/SendEmails.js
+++ frontend/src/components/sendEmails/SendEmails.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const SendEmails = () => {
-    return (
-        <div>
-            This is the SendEmails.
-        </div>
-    )
-}
+  const [email, setEmail] = useState({
+    to: "",
+    subject: "",
+    contents: "",
+  });
+  const handleChange = (event) => {
+    setEmail({ ...email, [event.target.name]: event.target.value });
+  };
+
+  return (
+    <div>
+      <div>
+        To: Subject: Contents:
+        <form>
+          <input
+            type='email'
+            placeholder='Send To'
+            name='to'
+            required
+            value={email.to}
+            onChange={(event) => handleChange(event)}
+          />
+          <input
+            type='subject'
+            placeholder='Enter subject'
+            name='subject'
+            required
+            value={email.subject}
+            onChange={(event) => handleChange(event)}
+          />
+          <textarea
+            type='text'
+            placeholder='Write email..'
+            name='contents'
+            required
+            value={email.contents}
+            onChange={(event) => handleChange(event)}
+          />
+          <button>Send Email</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default SendEmails;
